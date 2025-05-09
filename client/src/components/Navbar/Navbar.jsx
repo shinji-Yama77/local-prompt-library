787eb61f98d7fd34a621ddc1851c2dc2c2c778d4
Shinji Y <shinchan396@gmail.com>
2025-05-08 20:13:29 -0700

    added active highlighting of tabs

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Search, Code, Folder, PlusCircle, Settings, Moon, User } from 'lucide-react';
+import { useState } from 'react';
 
 const sideNavItems = [
     { id: 'all', icon: <Code size={20} />, label: 'All Snippets' },
@@ -11,13 +12,18 @@ const sideNavColor = "#0f172a";
 
 
 export default function Navbar() {
+    const [active, setActiveTab] = useState()
+
+
     return (
         <nav className="flex-1 overflow-y-auto p-2" style={{ backgroundColor: sideNavColor}}>
           <ul>
-            {sideNavItems.map(item => (
+            {sideNavItems.map((item) => (
               <li key={item.id}>
                 <button
-                  className="w-full flex items-center p-3 mb-1 rounded-md transition-colors" style={{ backgroundColor: sideNavColor}}
+                  className={`w-full flex items-center p-3 mb-1 rounded-md transition-colors ${
+                    active === item.id ? 'bg-blue-800 text-blue-100' : 'hover:bg-slate-800 text-slate-300'}`}
+                    onClick={() => setActiveTab(item.id)}
                 >
                   <span className="mr-3">{item.icon}</span>
                   <span>{item.label}</span>
